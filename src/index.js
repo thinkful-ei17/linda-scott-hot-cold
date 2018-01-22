792fbd95b7a236aa76c9b877a6f567ad4dd071f0
Linda Foo <linda.foo.dev@gmail.com>
2018-01-22 12:16:22 -0800

    set up project, Hello World! test done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GamePage from './components/gamepage';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <GamePage />,
+  document.getElementById('root')
+);
+
+
